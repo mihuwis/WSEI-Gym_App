e066c6441626d78c7c8b6ae999b1b6b0ca3b092f
mihuwis <mihuwis@gmail.com>
2024-06-27 21:19:49 +0200

    Add Ussername in Login

---
+++ GymApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace GymApp.ViewModels
+{
+    public class LoginViewModel : ObservableObject
+    {
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set => SetProperty(ref _username, value);
+        }
+    }
+}
