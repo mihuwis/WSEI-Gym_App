d02fc8f1b62680ad457c8481ae379225bbb95d69
mihuwis <mihuwis@gmail.com>
2024-06-27 19:06:47 +0200

    Add Data

---
+++ GymApp/Data/GymAppContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GymApp.Models;
+
+namespace GymApp.Data
+{
+    public class GymAppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=gymapp.db");
+        }
+    }
+}
