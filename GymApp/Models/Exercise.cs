81783d8d80e3d247d7748cfb3172e3ac4a1886fe
mihuwis <mihuwis@gmail.com>
2024-06-27 19:03:15 +0200

    Add Model

---
+++ GymApp/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Repetitions { get; set; }
+        public int WorkoutId { get; set; }
+        public Workout Workout { get; set; }
+    }
+}
---
+++ GymApp/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public List<Exercise> Exercises { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
